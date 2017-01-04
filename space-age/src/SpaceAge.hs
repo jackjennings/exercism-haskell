3f081f4a5f82be2ea0bd31183898eabe1e25366a
Jack Jennings <jack@standard-library.com>
2017-01-03 19:08:28 -0800

    First attempt at space-age

---
+++ space-age/src/SpaceAge.hs
@@ -0,0 +1,13 @@
+module SpaceAge (Planet(..), ageOn) where
+
+data Planet = Earth | Mercury | Venus | Mars | Jupiter | Saturn | Uranus | Neptune
+
+ageOn :: Planet -> Float -> Float
+ageOn Earth seconds = seconds / 31557600.0
+ageOn Mercury seconds = seconds / 31557600.0 / 0.2408467
+ageOn Venus seconds = seconds / 31557600.0 / 0.61519726
+ageOn Mars seconds = seconds / 31557600.0 / 1.8808158
+ageOn Jupiter seconds = seconds / 31557600.0 / 11.862615
+ageOn Saturn seconds = seconds / 31557600.0 / 29.447498
+ageOn Uranus seconds = seconds / 31557600.0 / 84.016846
+ageOn Neptune seconds = seconds / 31557600.0 / 164.79132
